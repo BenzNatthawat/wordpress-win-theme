df142461d8b320b46ef7b605a04140becb096cb1
Natthawat Singkala <you@example.com>
2020-09-27 00:40:24 +0700

    27/9v.1

--- functions.php
+++ functions.php
@@ -145,7 +145,10 @@ function underscores_scripts() {
 	wp_enqueue_style( 'style-style', get_template_directory_uri() . '/css/style.css', array(), _S_VERSION, false );
 	wp_enqueue_style( 'bootstrapcdn-style', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css', array(), _S_VERSION, false );
 	wp_enqueue_style( 'fonts-style', 'https://fonts.googleapis.com/css?family=Kanit:400,300&subset=thai,latin', array(), _S_VERSION, false );
-	wp_style_add_data( 'underscores-style', 'rtl', 'replace' );
+  wp_enqueue_style( 'twabc-1', get_template_directory_uri() . '/css/twabc-advanced.css', array(), _S_VERSION, false );
+	wp_enqueue_style( 'twabc-3', get_template_directory_uri() . '/css/twabc-advanced-3.css', array(), _S_VERSION, false );
+	wp_enqueue_style( 'twabc-4', get_template_directory_uri() . '/css/twabc-advanced-4.css', array(), _S_VERSION, false );
+wp_style_add_data( 'underscores-style', 'rtl', 'replace' );
   
 	wp_enqueue_script( 'jquery-script', get_template_directory_uri() . '/js/jquery.js', array(), _S_VERSION, false );
 	wp_enqueue_script( 'bootstrap-script', get_template_directory_uri() . '/js/bootstrap.js', array(), _S_VERSION, false );
@@ -185,3 +188,70 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
+
+
+/* ************************************* */ 
+define( 'TWABC_PLUGIN', __FILE__ );
+define( 'TWABC_PLUGIN_BASENAME', plugin_basename( TWABC_PLUGIN ) );
+// Advanced Bootstrap Carousel custom post type
+add_action( 'init', 'twabc_post_type');
+function twabc_post_type() {
+
+	$labels = array(
+		'name'                => 'Advanced Bootstrap Carousel',
+		'singular_name'       => 'Advanced Bootstrap Carousel',
+		'menu_name'           => 'ABC Slider',
+		'parent_item_colon'   => 'Parent:',
+		'all_items'           => 'All Carousels',
+		'view_item'           => 'View Carousel',
+		'add_new_item'        => 'Add New Carousel',
+		'add_new'             => 'Add New Carousel',
+		'edit_item'           => 'Edit Carousel',
+		'update_item'         => 'Update Carousel',
+		'search_items'        => 'Search Carousels',
+		'not_found'           => 'Carousel Not found',
+		'not_found_in_trash'  => 'Carousel Not found in Trash',
+	);
+	$args = array(
+		'labels' 				=> $labels,
+		'public' 				=> true,
+		'exclude_from_search' 	=> true,
+		'publicly_queryable' 	=> false,
+		'show_ui' 				=> true, 
+		'show_in_menu' 			=> true, 
+		'query_var' 			=> true,
+		'rewrite' 				=> true,
+		'capability_type' 		=> 'page',
+		'has_archive' 			=> true, 
+		'hierarchical'			=> false,
+		'menu_position' 		=> 20,
+        'menu_icon'           	=> 'dashicons-slides',
+		'supports' 				=> array('title', 'excerpt', 'thumbnail', 'page-attributes')
+	);
+	register_post_type( 'twabc', $args );
+}
+// Create a taxonomy for the carousel post type
+function twabc_taxonomies () {
+	$args = array('hierarchical' => true);
+	register_taxonomy( 'twabc_category', 'twabc', $args );
+}
+
+add_action( 'init', 'twabc_taxonomies', 0 );
+
+function twabc_addImageSupport() {
+	$supportedTypes = get_theme_support( 'post-thumbnails' );
+	if( $supportedTypes === false ) {
+		add_theme_support( 'post-thumbnails', array( 'twabc' ) );	  
+		add_image_size('featured_preview', 100, 55, true);
+	} elseif( is_array( $supportedTypes ) ) {
+		$supportedTypes[0][] = 'twabc';
+		add_theme_support( 'post-thumbnails', $supportedTypes[0] );
+		add_image_size('featured_preview', 100, 55, true);
+	}
+}
+add_action( 'after_setup_theme', 'twabc_addImageSupport');
+
+
+require_once('carousel/twabc-admin-view.php');
+require_once('carousel/twabc-front-view.php');
+require_once('carousel/twabc-admin-settings.php');
